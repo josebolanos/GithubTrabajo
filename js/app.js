f15818567afea79f7c89de9a47a175db18edbaa9
jose <josebolanoslanzarote@gmail.com>
2021-01-27 20:00:20 +0000

    Empezamos el proyecto añadiendo alerta y captando parts del formulario

---
+++ js/app.js
@@ -0,0 +1,33 @@
+const formulario = document.querySelector('#formulario');
+const resultado = document.querySelector('#resultado');
+
+document.addEventListener('DOMContentLoaded', () => {
+    formulario.addEventListener('submit', validarBusqueda);
+})
+
+function validarBusqueda(e) {
+    e.preventDefault();
+
+    const busqueda = document.querySelector('#busqueda').value;
+
+    if(busqueda.length < 3) {
+        mostrarMensaje('Busqueda muy corta...añade mas informacion');
+    }
+}
+
+function mostrarMensaje(msg) {
+    const alertaPrevia = document.querySelector('.alerta');
+
+    if(!alertaPrevia) {
+        const alerta = document.createElement('div');
+        alerta.classList.add('bg-gray-100', 'p-3', 'text-center', 'mt-3', 'alerta' );
+        alerta.textContent = msg;
+    
+        formulario.appendChild(alerta);
+    
+        setTimeout(() => {
+            alerta.remove();
+        },3000);
+    }
+   
+}
